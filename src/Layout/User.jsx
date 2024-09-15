cc1b80b49ace7fba7ac86a416a4a3ef04ca26394
Dev-as-sayed <assayed277@gmail.com>
2024-09-16 00:24:36 +0600

    add some baseic stractur on this commit

---
+++ src/Layout/User.jsx
@@ -0,0 +1,16 @@
+import { Layout } from "antd";
+import { Content, Footer, Header } from "antd/es/layout/layout";
+
+const User = () => {
+  return (
+    <div>
+      <Layout>
+        <Header>Header</Header>
+        <Content>Content</Content>
+        <Footer>Footer</Footer>
+      </Layout>
+    </div>
+  );
+};
+
+export default User;
