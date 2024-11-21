b3f1514d9a766b8d49e138c0a7547c951023dd6e
Dev-as-sayed <assayed277@gmail.com>
2024-11-21 22:31:13 +0600

    test server

--- src/hooks/AxiosPublic/useAxiosPublic.js
+++ src/hooks/AxiosPublic/useAxiosPublic.js
@@ -13,7 +13,8 @@
 import axios from "axios";
 
 const axiosPublic = axios.create({
-  baseURL: "https://api.allrooftakeoffs.com.au",
+  // baseURL: "https://api.allrooftakeoffs.com.au",
+  baseURL: "http://localhost:5000",
   withCredentials: true, // Automatically send cookies with cross-origin requests
 });
 
@@ -22,7 +23,8 @@ axiosPublic.interceptors.request.use(
     // Add headers dynamically here
     config.headers = {
       "Content-Type": "application/json", // Ensuring JSON content type
-      Origin: "https://api.allrooftakeoffs.com.au", // Dynamically set the origin from environment variable
+      // Origin: "https://api.allrooftakeoffs.com.au", // Dynamically set the origin from environment variable
+      Origin: "http://localhost:5000", // Dynamically set the origin from environment variable
       "Access-Control-Allow-Credentials": "true", // Allow credentials to be included in the request
       credentials: "include",
     };
